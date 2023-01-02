bb9bbb9e63135ef2b300aefce279d69473a8dde6
mzastsenski <koljan_z@yahoo.de>
2023-01-02 21:06:47 +0100

    changed

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const app = express();
-const path = require("path");
+// const path = require("path");
 const PORT = process.env.PORT || 4000;
 
 app.use(cors());
@@ -30,9 +30,6 @@ app.use(cookieParser());
 app.use(require("./requests.js"));
 app.use(require("./authentication.js"));
 
-app.get("*", (req, res) => {
-  // res.sendFile(path.resolve(__dirname, "index.html"));
-  // res.redirect('/index.html');
-});
+app.get("*", (req, res) => res.redirect("/"));
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
